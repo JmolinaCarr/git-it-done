e3cd40a04064682c832ed9c083d2d65fde7b1427
Jorge Molina <jorge.e.molina@hotmail.com>
2021-12-29 18:38:59 -0600

    updating

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,37 @@
-var getUserRepos = function(user) {
-    //format the github api url for
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-
-    //make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
-        });     
-    });
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
 };
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var username = nameInputEl.value.trim();
+        if (usename) {
+            getUserRepos(username);
+            nameInputEl.value = "";
+        }else {
+            alert("Please enter a Github username")
+        }
+    console.log(event);
 
-getUserRepos("facebook")
+userFormEl.addEventListener("submit", formSubmitHandler);
+}
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a get request to url
+    fetch(apiUrl).then(function(response) {
+      console.log(response);
+      response.json().then(function(data) {
+        displayRepos(data, user);
+        repoContainerEl.textContent = "";
+        repoSearchTerm.textContent = searchTerm;
+      });
+    });
+  };
+  
+  getUserRepos("jmolinacarr");
